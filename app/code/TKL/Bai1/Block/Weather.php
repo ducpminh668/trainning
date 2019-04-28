e96e688ccdde734780a56da9bd13fe89bf25774c
ducpminh668 <ducpminh668@gmail.com>
2019-04-28 16:42:53 +0700

    moule hello

---
+++ app/code/TKL/Bai1/Block/Weather.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TKL\Bai1\Block;
+
+class Weather extends \Magento\Framework\View\Element\Template
+{
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
--- app/code/TKL/Bai1/Controller/Hello/World.php
+++ app/code/TKL/Bai1/Controller/Hello/World.php
@@ -2,10 +2,20 @@
 
 namespace TKL\Bai1\Controller\Hello;
 
+use \Magento\Framework\App\Action\Context;
+use \Magento\Framework\View\Result\PageFactory;
+
 class World extends \Magento\Framework\App\Action\Action {
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
-        echo "Hello wolrd";
-        exit;
+        $this->pageFactory->create();
     }
 }
