5b7611f8ffc8055654368da4aa18911b2dcd690b
ducpminh668 <ducpminh668@gmail.com>
2019-04-28 15:59:59 +0700

    moule hello

---
+++ app/code/TKL/Bai1/Controller/Hello/World.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TKL\Bai1\Controller\Hello;
+
+class World extends \Magento\Framework\App\Action\Action {
+    public function execute()
+    {
+        echo "Hello wolrd";
+        exit;
+    }
+}
